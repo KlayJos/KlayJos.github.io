b9cf62e3db768034e9ef5513101480591de6cae7
KlayJos <93369665+KlayJos@users.noreply.github.com>
2025-05-10 10:06:40 +0000

    update

--- aooboo/.sh
+++ aooboo/.sh
@@ -1,5 +1,19 @@
+#Github一鍵push#
+
+
+#Linux終端機版 文件副檔名:.sh
 #!/bin/bash
 git add .
 read -p "請輸入Commit內容:" msg
 git commit -m "$msg"
 git push
+
+
+#Windows終端機版 文件副檔名:.bat
+#@echo off
+#cd /d %~dp0
+#set /p msg=請輸入Commit內容:
+#git add .
+#git commit -m "%msg%"
+#git push
+#pause
