258a9e368fb095e6305da523e5d3e2c03c69cc81
KlayJos <93369665+KlayJos@users.noreply.github.com>
2025-05-11 11:52:03 +0000

    update

---
+++ aooboo/js/style.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const headerHeight = document.querySelector('header').offsetHeight;
+    document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+  });
