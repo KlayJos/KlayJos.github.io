31457118dffefee74825a30acf13cbb197ee9fb2
KlayJos <93369665+KlayJos@users.noreply.github.com>
2025-05-11 18:58:16 +0000

    update

--- aooboo/js/main.js
+++ aooboo/js/main.js
@@ -1,3 +1,6 @@
+// index.html
+
+//location.reload();  //刷新頁面
 window.addEventListener('DOMContentLoaded', () => {
   const headerHeight = document.querySelector('header').offsetHeight;
   document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
