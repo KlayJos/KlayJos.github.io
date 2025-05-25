ac062bace63d9325b0e9d396f6a4bfdaa46e2fab
KlayJos <93369665+KlayJos@users.noreply.github.com>
2025-05-25 10:47:15 +0000

    update

--- aooboo/js/main.js
+++ aooboo/js/main.js
@@ -9,4 +9,23 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-let 
+window.addEventListener('DOMContentLoaded', () => { //加載監聽
+  const bleeding_true = document.querySelector('.bleeding_true');  //大出血_是 按鈕被點擊
+  if (bleeding_true){
+    bleeding_true.addEventListener('click', function(e) {
+      window.alert("請立即使用止血帶或加壓止血!\n如果情況嚴重或無法止血, 請嘗試止血並同時送醫!");
+    }, false);
+  }else{
+    console.error('元素 not found');
+  }
+
+  const bleeding_false = document.querySelector('.bleeding_false');  //大出血_否 按鈕被點擊
+  if (bleeding_false){
+    bleeding_false.addEventListener('click', function(e) {
+      window.alert("請繼續評估傷患: 意識、呼吸、脈搏。");
+    }, false);
+  }else{
+    console.error('元素 not found');
+  }
+
+});
