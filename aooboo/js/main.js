4de5634bc4c212efd3ac49355228d140a8e06e28
KlayJos <93369665+KlayJos@users.noreply.github.com>
2025-05-25 09:25:13 +0000

    update

--- aooboo/js/main.js
+++ aooboo/js/main.js
@@ -1,7 +1,12 @@
 // index.html
 
 //location.reload();  //刷新頁面
+
+//菜單欄高度適配:
 window.addEventListener('DOMContentLoaded', () => {
   const headerHeight = document.querySelector('header').offsetHeight;
   document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
 });
+
+
+let 
